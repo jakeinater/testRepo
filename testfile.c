7b8bc156f24e58bb36d61f6f390455ae42115c12
jakeinater <53323525+jakeinater@users.noreply.github.com>
2020-04-07 12:23:24 -0400

    Update testfile.c
    added a comma

--- testfile.c
+++ testfile.c
@@ -5,7 +5,7 @@ int main()
 {
 	char name[10];
 	scanf("%s", name);
-	printf("hello %s\nIt's nice to meet you!", name);
+	printf("hello %s,\nIt's nice to meet you!", name);
 	printf("\n\n this is in another paragraph");
 	return 0;
 }
